e34db71398ee9db7fd677a841054b9a617116669
Murillo Lannes <118365053+Mulannes@users.noreply.github.com>
2023-07-12 12:01:41 -0300

    Imgs and some Components

--- bombeiros/src/app/page.jsx
+++ bombeiros/src/app/page.jsx
@@ -1,7 +1,25 @@
-import Image from 'next/image'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import "./globals.css"
 
 export default function Home() {
   return (
-    <p className='text-slate-700 text-[96px] text-center'>Hello World</p>
-  )
+    <div className='flex items-center justify-center'>
+    <Card className="w-[500px] h-[700px]">
+      <CardHeader>
+          <CardTitle>
+          Hello World
+          </CardTitle>
+          <CardDescription>
+            AAAAAAAAA
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Mensagens</p>
+        </CardContent>
+        <CardFooter>
+          <form action=""></form>
+        </CardFooter>
+    </Card>
+    </div>
+      )
 }
