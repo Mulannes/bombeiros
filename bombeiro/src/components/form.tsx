d3ac084c30a0fb642a1cf084d9a43fe41ea3bf6a
Mulannes <mulannes@hotmail.com>
2023-08-14 21:02:05 -0300

    box shadow

--- bombeiro/src/components/form.tsx
+++ bombeiro/src/components/form.tsx
@@ -4,27 +4,42 @@ import React from 'react'
 export const Form = () => {
   return (
     <View style={Styles.container}>
-        <View style={Styles.navigation}>
-            <Text>Meu Deus</Text>
+        <View style={[Styles.card, Styles.shadowProp]}>
+            <View>
+            <Text style={Styles.heading}>
+                React Native Box Shadow (Shadow Props)
+            </Text>
+            </View>
+            <Text>
+            Using the elevation style prop to apply box-shadow for iOS devices
+            </Text>
         </View>
-    </View>
+      </View>
   )
 }
 
 const Styles = StyleSheet.create({
     container:{
-        flex: 1,
+
     },
-    navigation:{
-        width: 314,
-        height: 371,
-        backgroundColor: "#FFF",
-        shadowColor: '#000',
-        shadowOffset: {width: 4, height: 4},
-        shadowOpacity: 0.25,
-        shadowRadius: 20,
-        flex: 1,
-        alignContent: "center",
-        justifyContent: "center",
-    }
-})
+    heading: {
+      fontSize: 18,
+      fontWeight: '600',
+      marginBottom: 13,
+    },
+    card: {
+      backgroundColor: 'white',
+      borderRadius: 8,
+      paddingVertical: 45,
+      paddingHorizontal: 25,
+      width: '80%',
+      marginVertical: 10,
+    },
+    shadowProp: {
+      shadowColor: '#171717',
+      shadowOffset: {width: 4, height: 4},
+      shadowOpacity: 0.2,
+      shadowRadius: 3,
+    },
+  });
+  
