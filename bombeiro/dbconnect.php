8844d7f9e38b82d09abf744177c64d33cbc7b491
kauasdn <kauambassan10@hotmail.com>
2023-10-09 11:58:42 -0300

    conexão do login com o banco de dados

--- bombeiro/dbconnect.php
+++ bombeiro/dbconnect.php
@@ -1,12 +1,12 @@
 <?php
 
-    $servername = "localhost";
+    $host = "localhost";
     $username = "root";
     $password = "root";
-    $dbname = "bombeirosdb";
+    $database = "bombeirosdb";
 
     // cria a conexão
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    $conn = mysqli_connect($host, $username, $password, $database);
     // verifica a conexão
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
