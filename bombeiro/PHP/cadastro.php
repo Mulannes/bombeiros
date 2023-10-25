6418aeab18db3d4c399cf8ac08c853aaa5e2dab3
kauasdn <kauambassan10@hotmail.com>
2023-10-25 11:11:39 -0300

    updates no cadastro e login

--- bombeiro/PHP/cadastro.php
+++ bombeiro/PHP/cadastro.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro</title>
-    <link rel="stylesheet" href="cadastro.css">
+    <link rel="stylesheet" href="../CSS/cadastro.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <div class="container-fluid m-0 p-0"
-        style="background-image: url('images/cadastro.png'); background-size: cover; height: 100vh;">
+        style="background-image: url('../images/cadastro.png'); background-size: cover; height: 100vh;">
         <form action="processamento_cadastro.php" method="POST">
             <div class="form-row">
                 <div class="form-group col-md-6">
