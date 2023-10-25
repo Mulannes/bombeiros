88a4195f2f2a1314113a48a39d83f699ab14633a
Lucas Guszak <guszak123@gmail.com>
2023-10-25 10:47:44 -0300

    cadastro.php

---
+++ bombeiro/PHP/processamento_cadastro.php.php
@@ -0,0 +1,30 @@
+<?php
+include('dbconnect.php');
+
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+    $cpf = $_POST['CPF'];
+
+    $sql = "INSERT INTO usuario (nome_usuario, email_usuario, senha_usuario, CPF_usuario) VALUES (?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    
+    // Vincular os valores das variáveis à consulta preparada
+    $stmt->bind_param('ssss', $nome, $email, $senha, $cpf);
+
+    if ($stmt->execute()) {
+        // O INSERT foi bem-sucedido, você pode redirecionar para a página inicial
+        $_SESSION['email_usuario'] = $email;
+        header("Location: cadastro.php");
+        exit();
+    } else {
+        $error = "Erro ao inserir usuário no banco de dados: " . $stmt->error;
+    }
+}
+
+$conexao->close();
+?>
