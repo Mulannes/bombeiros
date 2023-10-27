254c2d5881a8fef384a93c5160a3978aad84d3a7
Mulannes <mulannes@hotmail.com>
2023-10-26 22:57:49 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- bombeiro/PHP/index.php
+++ bombeiro/PHP/index.php
@@ -8,50 +8,12 @@
     <link rel="stylesheet" href="../CSS/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="../components/nav-pills.js"></script>
-    <script src="../components/navbar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
 </head>
 
 <body>
     <div class="container-fluid m-0 p-0">
-        <header>
-            <div class="d-lg-none">
-
-                <nav class="navbar">
-                    <div class="container">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasNav">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <a href="../HTML/conta.html"><img src="../images/botãologin.png"></a>
-                    </div>
-                </nav>
-                <div class="offcanvas offcanvas-start" id="offcanvasNav">
-
-                    <div class="offcanvas-header">
-                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
-                            aria-label="Fechar"></button>
-                    </div>
-
-                    <div class="offcanvas-body">
-                        <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="../HTML/ocorrencia.html">Fazer Registro</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../HTML/registro.html">Registro</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../HTML/conta.html">Conta</a>
-                            </li>
-                        </ul>
-                    </div>
-
-                </div>
-
-            </div>
-        </header>
+        <navbar-1></navbar-1>
         <div class="container-fluid">
             <div class="row text">
                 <div class="col-lg">
