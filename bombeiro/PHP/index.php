ae6dda0be56ddde61ce2dfe48be5a27b73597602
mulannes <mulannes@hotmail.com>
2023-11-22 12:30:18 -0300

    ASADA

--- bombeiro/PHP/index.php
+++ bombeiro/PHP/index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="../CSS/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
@@ -19,10 +20,10 @@
             <nav class="navbar">
                 <div class="container">
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNav">
-                        <span class="navbar-toggler-icon"></span>
+                        data-bs-target="#offcanvasNav" style="width: 50px; height: 50px; position: relative; border: transparent;">
+                        <img src="../images/MenuIcon.png" class="navbar-toggler-icon" style="width: 58px; height: 58px; position: absolute;top: 0; left: 0; background-image: none;">
                     </button>
-                    <a href="conta.php"><img src="../images/botãologin.png"></a>
+                    <a href="../PHP/conta.php"><img src="../images/botãologin.png"></a>
                 </div>
             </nav>
             <div class="offcanvas offcanvas-start" id="offcanvasNav">
@@ -35,13 +36,13 @@
                 <div class="offcanvas-body">
                     <ul class="navbar-nav">
                         <li class="nav-item">
+                            <a class="nav-link" href="../PHP/index.php">Menu</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link" href="../HTML/ocorrencia.html">Fazer Registro</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="../HTML/registro.html">Ver Registro</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="conta.php">Conta</a>
+                            <a class="nav-link" href="../HTML/registro.html">Registro</a>
                         </li>
                     </ul>
                 </div>
@@ -53,8 +54,8 @@
         <div class="container-fluid">
             <div class="row text">
                 <div class="col-lg">
-                    <p class="display-1">Bombeiros,</p>
-                    <p class="display-3">Página inicial.</p>
+                    <p class="display-1" style="color: #000;font-family: Poppins;font-size: 48px;font-style: normal;font-weight: 700;line-height: normal; margin:0;">Bombeiros,</p>
+                    <p class="display-3" style="color: #000;font-family: Poppins;font-size: 44px;font-style: normal;font-weight: 500;line-height: normal;">Página inicial.</p>
                 </div>
             </div>
         </div>
@@ -69,7 +70,14 @@
             <div class="d-grid gap-2 d-md-flex justify-content-md-center">
 
                 <label class="btn btn-outline-danger" for="btnradio1" data-bs-target="#carouselExampleDark"
-                    data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1">Menu</label>
+                    data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1" style="border-radius: 10px;border: 1px solid rgba(0, 0, 0, 0.10);background: #FFF;
+"><p style="color: #000;
+text-align: center;
+font-family: Poppins;
+font-size: 14px;
+font-style: normal;
+font-weight: 600;
+line-height: normal; margin:0;">Menu</p></label>
 
                 <label class="btn btn-outline-danger" for="btnradio2" data-bs-target="#carouselExampleDark"
                     data-bs-slide-to="1" aria-label="Slide 2">Fazer Registro</label>
@@ -117,20 +125,28 @@
             <br>
             <br>
 
-            <ul class="nav nav-pills nav-justified">
-                <li class="nav-item">
-                    <img src="../images/home.png" class="rounded mx-auto d-block">
-                    <a class="nav-link active" aria-current="page" href="../PHP/index.php">Menu</a>
-                </li>
-                <li class="nav-item">
-                    <img src="../images/fazerregistro.png" class="rounded mx-auto d-block">
-                    <a class="nav-link" href="../HTML/ocorrencia.html">Fazer Registro</a>
-                </li>
-                <li class="nav-item">
-                    <img src="../images/registro.png" class="rounded mx-auto d-block">
-                    <a class="nav-link" href="../HTML/registro.html">Registros</a>
-                </li>
-            </ul>
+            <div style="width: 100%; display: flex; justify-content: center; margin-bottom: 20px;">
+            <div class="nav-pills-style" style="width: 95%; height: 75px; flex-shrink: 0; border-radius: 50px; background: #FFF; box-shadow: 0px 0px 100px 0px rgba(0, 0, 0, 0.50);">
+                <ul class="nav nav-pills nav-justified">
+                    <li class="nav-item" style=" max-height: 65px;">
+                        <img src="../images/homeR.png" class=" mx-auto d-block" style="padding: 10px;">
+                        <a class="nav-link" href="../PHP/index.php" style="color: #C21219; padding: 0; font-size: 14px;font-weight: bold; ">Menu</a>
+                    </li>
+                    <li class="nav-item" style="max-height: 65px;">
+                        <img src="../images/fazerregistro.png" class=" mx-auto d-block" style="padding: 10px 10px 5px 10px;"> 
+                        <a class="nav-link" href="../HTML/ocorrencia.html" style="color: black; padding: 0; font-size: 14px; height: 28px; line-height: 13px;">Fazer<br>Registro</a>
+                    </li>
+                    <li class="nav-item" style="max-height: 65px;">
+                        <img src="../images/registro.png" class=" mx-auto d-block" style="padding: 10px;">
+                        <a class="nav-link" href="../HTMl/registro.html" style="color: black; padding: 0; font-size: 14px">Registros</a>
+                    </li>
+                    <li class="nav-item" style="max-height: 65px;">
+                        <img src="../images/contaP.png" class=" mx-auto d-block" style="padding: 10px;">
+                        <a class="nav-link" href="../PHP/conta.php" style="color: black; padding: 0; font-size: 14px">Conta</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
 
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
