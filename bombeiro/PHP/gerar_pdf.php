cd48c7ce805d6582728ce63661ef9679b57b3032
Mulannes <mulannes@hotmail.com>
2023-12-04 19:07:24 -0300

    mais do relatorio

--- bombeiro/PHP/gerar_pdf.php
+++ bombeiro/PHP/gerar_pdf.php
@@ -6,6 +6,105 @@ include_once 'dbconnect.php';
 // Incluir a biblioteca TCPDF
 require_once('../TCPDF-main/tcpdf.php');
 
+// /////////////////////////////////////////
+// // Fichas
+// /////////////////////////////////////////
+
+// $query_ficha = "SELECT 
+//         id_fichas,	
+//         data_ficha,
+//         nome_paciente,
+//         idAnamnese_Emergencia_Medica,
+//     	idAnamnese_Gestacional,
+//     	idAvaliacao_Cinematica,
+//       	idFicha_Avaliacao_Glasgow,
+//      	idFicha_Localizacao_dos_Traumas,
+//         idMateriais_Utilizados_Deixados,
+//         idMateriais_Utilizados_Descartavel,
+//         idObjetos_Recolhidos,
+//         idObservacoes_Importantes,
+//         idFicha_Paciente,
+//         idProblemas_Encontrados,
+//         idProcedimentos_Efetuados,
+//         idSinais_e_Sintomas	,
+//         idFicha_Sinais_Vitais,
+//         idTermoRecusa,
+//         idTipo_de_Ocorrencia,	
+//         idFicha_Decisao_Transporte,	
+//         idDetalhes_Viagem,	
+//         idFicha_Forma_de_Conducao,	
+//         idFicha_Transporte_Vitima_Era,	
+//         id_usuario	
+
+//     FROM fichas 
+//     ORDER BY id_fichas DESC 
+//     LIMIT 1";
+
+// // Prepara a QUERY
+// $result_ficha = $conn->prepare($query_ficha);
+// // Executar a QUERY
+// $result_ficha->execute();
+
+// // Adicionar conteúdo ao PDF
+// $html = "<h1>Relatório</h1>";
+
+// // Ler os registros retornados do BD (ficha_tipo_de_ocorrencia)
+// $result_ficha->bind_result(
+//     $id_fichas,
+//     $data_ficha,
+//     $nome_paciente,
+//     $idAnamnese_Emergencia_Medica,
+//     $idAnamnese_Gestacional,
+//     $idAvaliacao_Cinematica,
+//     $idFicha_Avaliacao_Glasgow,
+//     $idFicha_Localizacao_dos_Traumas,
+//     $idMateriais_Utilizados_Deixados,
+//     $idMateriais_Utilizados_Descartavel,
+//     $idObjetos_Recolhidos,
+//     $idObservacoes_Importantes,
+//     $idFicha_Paciente,
+//     $idProblemas_Encontrados,
+//     $idProcedimentos_Efetuados,
+//     $idSinais_e_Sintomas,
+//     $idFicha_Sinais_Vitais,
+//     $idTermoRecusa,
+//     $idTipo_de_Ocorrencia,
+//     $idFicha_Decisao_Transporte,
+//     $idDetalhes_Viagem,
+//     $idFicha_Forma_de_Conducao,
+//     $idFicha_Transporte_Vitima_Era,
+//     $id_usuario
+// );
+
+
+// if ($result_ficha->fetch()) {
+//     $html .= "<p>data_ficha: {$data_ficha}</p>";
+//     $html .= "<p>nome_paciente: {$nome_paciente}</p>";
+//     $html .= "<p>idAnamnese_Emergencia_Medica: {$idAnamnese_Emergencia_Medica}</p>";
+//     $html .= "<p>idAnamnese_Gestacional: {$idAnamnese_Gestacional}</p>";
+//     $html .= "<p>idAvaliacao_Cinematica: {$idAvaliacao_Cinematica}</p>";
+//     $html .= "<p>idFicha_Avaliacao_Glasgow: {$idFicha_Avaliacao_Glasgow}</p>";
+//     $html .= "<p>idFicha_Localizacao_dos_Traumas: {$idFicha_Localizacao_dos_Traumas}</p>";
+//     $html .= "<p>idMateriais_Utilizados_Deixados: {$idMateriais_Utilizados_Deixados}</p>";
+//     $html .= "<p>idMateriais_Utilizados_Descartavel: {$idMateriais_Utilizados_Descartavel}</p>";
+//     $html .= "<p>idObjetos_Recolhidos: {$idObjetos_Recolhidos}</p>";
+//     $html .= "<p>idObservacoes_Importantes: {$idObservacoes_Importantes}</p>";
+//     $html .= "<p>idFicha_Paciente: {$idFicha_Paciente}</p>";
+//     $html .= "<p>idProblemas_Encontrados: {$idProblemas_Encontrados}</p>";
+//     $html .= "<p>idProcedimentos_Efetuados: {$idProcedimentos_Efetuados}</p>";
+//     $html .= "<p>idSinais_e_Sintomas: {$idSinais_e_Sintomas}</p>";
+//     $html .= "<p>idFicha_Sinais_Vitais: {$idFicha_Sinais_Vitais}</p>";
+//     $html .= "<p>idTermoRecusa: {$idTermoRecusa}</p>";
+//     $html .= "<p>idTipo_de_Ocorrencia: {$idTipo_de_Ocorrencia}</p>";
+//     $html .= "<p>idFicha_Decisao_Transporte: {$idFicha_Decisao_Transporte}</p>";
+//     $html .= "<p>idDetalhes_Viagem: {$idDetalhes_Viagem}</p>";
+//     $html .= "<p>idFicha_Forma_de_Conducao: {$idFicha_Forma_de_Conducao}</p>";
+//     $html .= "<p>idFicha_Transporte_Vitima_Era: {$idFicha_Transporte_Vitima_Era}</p>";
+//     $html .= "<p>id_usuario: {$id_usuario}</p>";
+//     // Adicione outros campos conforme necessário
+// }
+// $result_ficha->close();
+
 /////////////////////////////////////////
 // query_paciente
 /////////////////////////////////////////
@@ -31,6 +130,7 @@ $query_paciente = "SELECT
 $result_paciente = $conn->prepare($query_paciente);
 // Executar a QUERY
 $result_paciente->execute();
+
 
 // Configurar o PDF
 $pdf = new TCPDF();
@@ -69,7 +169,6 @@ while ($result_paciente->fetch()) {
     $html .= "<p>Idade Acompanhante: {$idade_acompanhante}</p>";
     $html .= "<p>Local da ocorrencia: {$local_da_ocorrencia}</p>";
 }
-
 
 /////////////////////////////////////////
 // query_ocorrenciaTipo
@@ -136,31 +235,80 @@ $result_ocorrenciaTipo->bind_result(
 
 $html .= "<h2>Detalhes da Ocorrência</h2>";
 
-if ($result_ocorrenciaTipo->fetch()) {
-    $html .= "<p>ID Tipo de Ocorrência: {$idTipo_de_Ocorrencia}</p>";
-    $html .= "<p>Causado Por Animais: {$Causado_Por_Animais}</p>";
-    $html .= "<p>Com Meio De Transporte: {$Com_Meio_De_Transporte}</p>";
-    $html .= "<p>Desmoronamento/Deslizamento: {$Desmoronamento_Deslizamento}</p>";
-    $html .= "<p>Emergencia_Medica: {$Emergencia_Medica}</p>";
-    $html .= "<p>Queda_De_Altura_2M: {$Queda_De_Altura_2M}</p>";
-    $html .= "<p>Tentativa_De_Suicidio: {$Tentativa_De_Suicidio}</p>";
-    $html .= "<p>Queda_Propria_Altura: {$Queda_Propria_Altura}</p>";
-    $html .= "<p>Afogamento: {$Afogamento}</p>";
-    $html .= "<p>Agressao: {$Agressao}</p>";
-    $html .= "<p>Atropelamento: {$Atropelamento}</p>";
-    $html .= "<p>Choque_Eletrico: {$Choque_Eletrico}</p>";
-    $html .= "<p>Desabamento: {$Desabamento}</p>";
-    $html .= "<p>Domestico: {$Domestico}</p>";
-    $html .= "<p>Esportivo: {$Esportivo}</p>";
-    $html .= "<p>Intoxicacao: {$Intoxicacao}</p>";
-    $html .= "<p>Queda_Bicicleta: {$Queda_Bicicleta}</p>";
-    $html .= "<p>Queda_Moto: {$Queda_Moto}</p>";
-    $html .= "<p>Queda_Nivel_2M: {$Queda_Nivel_2M}</p>";
-    $html .= "<p>Trabalho: {$Trabalho}</p>";
-    $html .= "<p>Transferencia: {$Transferencia}</p>";
-    $html .= "<p>Outro_Campo: {$Outro_Campo}</p>";
-    // Adicione outros campos conforme necessário
+while ($result_ocorrenciaTipo->fetch()) {
+    $nonNullFields = [];
+    if ($idTipo_de_Ocorrencia !== null) {
+        $nonNullFields[] = "ID Tipo de Ocorrência: {$idTipo_de_Ocorrencia}";
+    }
+    if ($Causado_Por_Animais !== null) {
+        $nonNullFields[] = "Tipo de Causado_Por_Animais: {$Causado_Por_Animais}";
+    }
+    if ($Com_Meio_De_Transporte !== null) {
+        $nonNullFields[] = "Com_Meio_De_Transporte: {$Com_Meio_De_Transporte}";
+    }
+    if ($Desmoronamento_Deslizamento !== null) {
+        $nonNullFields[] = "Desmoronamento_Deslizamento: {$Desmoronamento_Deslizamento}";
+    }
+    if ($Emergencia_Medica !== null) {
+        $nonNullFields[] = "Emergencia_Medica: {$Emergencia_Medica}";
+    }
+    if ($Queda_De_Altura_2M !== null) {
+        $nonNullFields[] = "Queda_De_Altura_2M: {$Queda_De_Altura_2M}";
+    }
+    if ($Tentativa_De_Suicidio !== null) {
+        $nonNullFields[] = "Tentativa_De_Suicidio: {$Tentativa_De_Suicidio}";
+    }
+    if ($Queda_Propria_Altura !== null) {
+        $nonNullFields[] = "Queda_Propria_Altura: {$Queda_Propria_Altura}";
+    }
+    if ($Afogamento !== null) {
+        $nonNullFields[] = "Afogamento: {$Afogamento}";
+    }
+    if ($Agressao !== null) {
+        $nonNullFields[] = "Agressao: {$Agressao}";
+    }
+    if ($Atropelamento !== null) {
+        $nonNullFields[] = "Atropelamento: {$Atropelamento}";
+    }
+    if ($Atropelamento !== null) {
+        $nonNullFields[] = "Atropelamento: {$Atropelamento}";
+    }
+    if ($Choque_Eletrico !== null) {
+        $nonNullFields[] = "Choque_Eletrico: {$Choque_Eletrico}";
+    }
+    if ($Desabamento !== null) {
+        $nonNullFields[] = "Desabamento: {$Desabamento}";
+    }
+    if ($Domestico !== null) {
+        $nonNullFields[] = "Domestico: {$Domestico}";
+    }
+    if ($Esportivo !== null) {
+        $nonNullFields[] = "Esportivo: {$Esportivo}";
+    }
+    if ($Intoxicacao !== null) {
+        $nonNullFields[] = "Intoxicacao: {$Intoxicacao}";
+    }
+    if ($Queda_Bicicleta !== null) {
+        $nonNullFields[] = "Queda_Bicicleta: {$Queda_Bicicleta}";
+    }
+    if ($Queda_Moto !== null) {
+        $nonNullFields[] = "Queda_Moto: {$Queda_Moto}";
+    }
+    if ($Queda_Nivel_2M !== null) {
+        $nonNullFields[] = "Queda_Nivel_2M: {$Queda_Nivel_2M}";
+    }
+    if ($Trabalho !== null) {
+        $nonNullFields[] = "Trabalho: {$Trabalho}";
+    }
+    if ($Transferencia !== null) {
+        $nonNullFields[] = "Transferencia: {$Transferencia}";
+    }
+    if ($Outro_Campo !== null) {
+        $nonNullFields[] = "Outro_Campo: {$Outro_Campo}";
+    }
+    $html .= implode('<br>', $nonNullFields);
 }
+
 $result_ocorrenciaTipo->close();
 
 /////////////////////////////////////////
--- bombeiro/PHP/registro.php
+++ bombeiro/PHP/registro.php
@@ -23,8 +23,14 @@ if (!isset($_SESSION['loggedIn'])) {
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="/bombeiro/components/navbar.js"></script>
     <style>
-        .dropdown-toggle::after{margin: 0; transition: .5s;}
-        .dropdown-toggle:hover::after{transform: rotate(180deg);}
+        .dropdown-toggle::after {
+            margin: 0;
+            transition: .5s;
+        }
+
+        .dropdown-toggle:hover::after {
+            transform: rotate(180deg);
+        }
     </style>
 </head>
 
@@ -32,26 +38,25 @@ if (!isset($_SESSION['loggedIn'])) {
     <header>
         <div class="d-lg-none">
 
-        <nav class="navbar">
-                    <div class="container">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                            data-bs-target="#offcanvasNav"
-                            style="width: 50px; height: 50px; position: relative; border: transparent;">
-                            <img src="../images/MenuIcon.png" class="navbar-toggler-icon"
-                                style="width: 58px; height: 58px; position: absolute; top: 0; left: 0; background-image: none;">
+            <nav class="navbar">
+                <div class="container">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNav"
+                        style="width: 50px; height: 50px; position: relative; border: transparent;">
+                        <img src="../images/MenuIcon.png" class="navbar-toggler-icon"
+                            style="width: 58px; height: 58px; position: absolute; top: 0; left: 0; background-image: none;">
+                    </button>
+                    <form method="post" action="admin/redirectadm.php">
+                        <button type="submit" name="btnRedirect" class="btn btn">
+                            <img src="../images/botãologin.png" alt="Botão de Login">
                         </button>
-                        <form method="post" action="admin/redirectadm.php">
-                            <button type="submit" name="btnRedirect" class="btn btn">
-                                <img src="../images/botãologin.png" alt="Botão de Login">
-                            </button>
-                        </form>
-                    </div>
-                </nav>
+                    </form>
+                </div>
+            </nav>
             <div class="offcanvas offcanvas-start" id="offcanvasNav">
 
                 <div class="offcanvas-header">
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
-                        aria-label="Fechar"></button>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Fechar"></button>
                 </div>
 
                 <div class="offcanvas-body">
@@ -81,7 +86,8 @@ if (!isset($_SESSION['loggedIn'])) {
             </div>
         </div>
         <div class="container-align" style="display: flex; flex-direction: column; align-items: center;gap: 6px;">
-            <div class="borda" style="width: 95%; border-radius: 10px; height: 35px; display: flex; align-items: center; color: #000;font-family: Poppins;font-size: 14px;font-style: normal;font-weight: 700;line-height: normal;">
+            <div class="borda"
+                style="width: 95%; border-radius: 10px; height: 35px; display: flex; align-items: center; color: #000;font-family: Poppins;font-size: 14px;font-style: normal;font-weight: 700;line-height: normal;">
                 <div class="container">
                     <div class="row align-items-start">
                         <div class="col text-center">
@@ -96,32 +102,32 @@ if (!isset($_SESSION['loggedIn'])) {
                     </div>
                 </div>
             </div>
-<?php
-    // Suponha que você tenha a informação se o usuário é um administrador em $isAdmin
-    $id_usuario = $_SESSION['id_usuario'];
-    $isAdmin = isset($_SESSION['admin']) && $_SESSION['admin'] === true;
+            <?php
+            // Suponha que você tenha a informação se o usuário é um administrador em $isAdmin
+            $id_usuario = $_SESSION['id_usuario'];
+            $isAdmin = isset($_SESSION['admin']) && $_SESSION['admin'] === true;
 
-    if ($isAdmin) {
-        // Se o usuário for um administrador, consulta sem a condição WHERE
-        $sql = "SELECT * FROM fichas";
-        $stmt = $conn->prepare($sql);
-    } else {
-        // Se não for um administrador, consulta com a condição WHERE
-        $sql = "SELECT * FROM fichas WHERE id_usuario = ?";
-        $stmt = $conn->prepare($sql);
-        $stmt->bind_param('i', $id_usuario);
-    }
+            if ($isAdmin) {
+                // Se o usuário for um administrador, consulta sem a condição WHERE
+                $sql = "SELECT * FROM fichas";
+                $stmt = $conn->prepare($sql);
+            } else {
+                // Se não for um administrador, consulta com a condição WHERE
+                $sql = "SELECT * FROM fichas WHERE id_usuario = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param('i', $id_usuario);
+            }
 
-    if ($stmt) {
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
+            if ($stmt) {
+                if ($stmt->execute()) {
+                    $result = $stmt->get_result();
 
-            while ($row = $result->fetch_assoc()) {
-                $id = $row['id_fichas'];
-                $nome = $row['nome_paciente'];
+                    while ($row = $result->fetch_assoc()) {
+                        $id = $row['id_fichas'];
+                        $nome = $row['nome_paciente'];
 
-                // Imprima o HTML formatado com os dados do banco de dados
-                echo '<div class="borda" style="width: 95%; border-radius: 10px; height: 50px; display: flex; align-items: center;">
+                        // Imprima o HTML formatado com os dados do banco de dados
+                        echo '<div class="borda" style="width: 95%; border-radius: 10px; height: 50px; display: flex; align-items: center;">
                         <div class="container">
                             <div class="row align-items-center">
                                 <div class="col text-center">
@@ -144,55 +150,61 @@ if (!isset($_SESSION['loggedIn'])) {
                             </div>
                         </div>
                     </div>';
+                    }
+                } else {
+                    echo "Erro na consulta: " . $stmt->error;
+                }
+
+                $stmt->close();
+            } else {
+                echo "Erro na preparação da consulta: " . $conn->error;
             }
-        } else {
-            echo "Erro na consulta: " . $stmt->error;
-        }
 
-        $stmt->close();
-    } else {
-        echo "Erro na preparação da consulta: " . $conn->error;
-    }
+            $conn->close();
+            ?>
 
-    $conn->close();
-?>
-
-    <br>
-    <div style="width: 100%; display: flex; justify-content: center; bottom: 15px;">
-        <div class="nav-pills-style" style="width: 95%; height: 75px; flex-shrink: 0; border-radius: 50px; background: #FFF; box-shadow: 0px 0px 100px 0px rgba(0, 0, 0, 0.50);">
-            <ul class="nav nav-pills nav-justified">
-                <li class="nav-item" style=" max-height: 65px;">
-                    <img src="../images/home.png" class=" mx-auto d-block" style="padding: 10px;">
-                    <a class="nav-link" href="index.php" style="color: black; padding: 0; font-size: 14px">Menu</a>
-                </li>
-                <li class="nav-item" style="max-height: 65px;">
-                    <img src="../images/fazerregistro.png" class=" mx-auto d-block" style="padding: 10px 10px 5px 10px;"> 
-                    <a class="nav-link" href="ocorrencia.php" style="color: black; padding: 0; font-size: 14px; height: 28px; line-height: 13px;">Fazer<br>Registro</a>
-                </li>
-                <li class="nav-item" style="max-height: 65px;">
-                    <img src="../images/registroR.png" class=" mx-auto d-block" style="padding: 10px;">
-                    <a class="nav-link" href="registro.php" style="color: #C21219; padding: 0; font-size: 14px;font-weight: bold;">Registros</a>
-                </li>
-                <li class="nav-item" style="max-height: 65px;">
-                <form action="admin/redirectadm.php" method="post">
-                    <img src="../images/contaP.png" class=" mx-auto d-block" style="padding: 10px;">
-                    <button type="submit" name="btnRedirect" style="color: #black; padding: 0; font-size: 14px; background-color: transparent; border: none;">Conta</button>
-                </form>
-                </li>
-            </ul>
+            <br>
+            <div style="width: 100%; display: flex; justify-content: center; bottom: 60px; position:absolute;">
+                <div class="nav-pills-style"
+                    style="width: 95%; height: 75px; flex-shrink: 0; border-radius: 50px; background: #FFF; box-shadow: 0px 0px 100px 0px rgba(0, 0, 0, 0.50);">
+                    <ul class="nav nav-pills nav-justified">
+                        <li class="nav-item" style=" max-height: 65px;">
+                            <img src="../images/home.png" class=" mx-auto d-block" style="padding: 10px;">
+                            <a class="nav-link" href="index.php"
+                                style="color: black; padding: 0; font-size: 14px">Menu</a>
+                        </li>
+                        <li class="nav-item" style="max-height: 65px;">
+                            <img src="../images/fazerregistro.png" class=" mx-auto d-block"
+                                style="padding: 10px 10px 5px 10px;">
+                            <a class="nav-link" href="ocorrencia.php"
+                                style="color: black; padding: 0; font-size: 14px; height: 28px; line-height: 13px;">Fazer<br>Registro</a>
+                        </li>
+                        <li class="nav-item" style="max-height: 65px;">
+                            <img src="../images/registroR.png" class=" mx-auto d-block" style="padding: 10px;">
+                            <a class="nav-link" href="registro.php"
+                                style="color: #C21219; padding: 0; font-size: 14px;font-weight: bold;">Registros</a>
+                        </li>
+                        <li class="nav-item" style="max-height: 65px;">
+                            <form action="admin/redirectadm.php" method="post">
+                                <img src="../images/contaP.png" class=" mx-auto d-block" style="padding: 10px;">
+                                <button type="submit" name="btnRedirect"
+                                    style="color: #black; padding: 0; font-size: 14px; background-color: transparent; border: none;">Conta</button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
 
-    <footer class="text-center text-lg-start">
-        <div class="text-center p-3">
-            &copy; 2023 Noar. Todos os direitos reservados.
-        </div>
-      </footer>
+        <footer class="text-center text-lg-start">
+            <div class="text-center p-3" style="bottom: 10px; position:absolute;">
+                &copy; 2023 Noar. Todos os direitos reservados.
+            </div>
+        </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+            </script>
 </body>
 
 </html>
@@ -204,12 +216,14 @@ if (!isset($_SESSION['loggedIn'])) {
             $.ajax({
                 type: 'POST',
                 url: 'excluir_ficha.php', // Substitua pelo script que processa a exclusão
-                data: { id: id },
-                success: function(response) {
+                data: {
+                    id: id
+                },
+                success: function (response) {
                     // Lide com a resposta, talvez recarregando a página ou atualizando a lista de fichas
                     console.log(response);
                 },
-                error: function(error) {
+                error: function (error) {
                     console.error(error);
                 }
             });
