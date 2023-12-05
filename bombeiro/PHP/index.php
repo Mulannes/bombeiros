034e898d893191e55514c675d73c8e489e068574
kauasdn <kauambassan10@hotmail.com>
2023-12-04 23:36:52 -0300

    tirei o gerar relatorio do index

--- bombeiro/PHP/index.php
+++ bombeiro/PHP/index.php
@@ -197,7 +197,6 @@ if (!isset($_SESSION['loggedIn'])) {
             </div>
 
         </div>
-        <a href="gerar_pdf.php">Gerar relatorio</a>
 
         <footer class="text-center text-lg-start">
             <div class="text-center p-3">
